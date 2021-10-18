5a637b70c21e7059d0ef74e07f4a60576dc15b66
Percy Mohale <46670439+percymohale@users.noreply.github.com>
2021-10-18 22:49:55 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket  = "terraform-state-percy-explore"
-    key     = "test/terraform.tfstate"
-    region  = "eu-west-1"
-    profile = "msp-dev"
+  backend "remote" {
+     hostname = "app.terraform.io"
+    organization = "Terrworkspaceorg"
+
+    workspaces {
+      name = "terraform-workspaces"
+    }
   }
 }
- 
