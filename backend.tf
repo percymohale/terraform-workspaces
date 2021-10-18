ca008447df327ef813f7fa940c9a5e885f0e56a6
Percy <percymohale9@gmail.com>
2021-10-18 20:48:21 +0200

    added the ami

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-percy-explore"
-    key     = "terraform.tfstate"
-    region  = var.region
+# terraform {
+#   backend "s3" {
+#     bucket  = "terraform-state-percy-explore"
+#     key     = "terraform.tfstate"
+#     region  = var.region
     
-  }
-}
+#   }
+# }
--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myserver" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = var.instance_ami
   instance_type = var.instance_type
 
   tags = {
